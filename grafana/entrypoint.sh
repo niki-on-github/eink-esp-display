02117572bb3fb7985f85b60243d5ded21bdd84e6
nix <nix@local>
2025-10-06 20:26:35 +0200

    allow specify Name

--- grafana/entrypoint.sh
+++ grafana/entrypoint.sh
@@ -7,7 +7,7 @@ if [ -z "${IMAGE_URL}" ]; then
 fi
 
 while true; do
-    python main.py ${IMAGE_URL} /out/data --crop-top ${CROP_TOP:-0} --timeline ${TIMELINE:-3600} --threshold ${TRESHOLD:-100}
+    python main.py ${IMAGE_URL} /out/${NAME:-data} --crop-top ${CROP_TOP:-0} --timeline ${TIMELINE:-3600} --threshold ${TRESHOLD:-100}
     echo "sleep ${UPDATE_INTERVAL_IN_SECONDS:-3600}"
     sleep ${UPDATE_INTERVAL_IN_SECONDS:-3600}
 done
